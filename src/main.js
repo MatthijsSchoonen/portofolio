f5096a93c8688e422306b4d737dcf63ce048e793
MATTHlJS <113593159+MATTHlJS@users.noreply.github.com>
2024-05-09 19:40:15 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { dialogueData,scaleFactor } from "./constants.js";
-import { k } from "./kaboomCtx.js";
-import { displayDialogue, setCamScale } from "./utils.js";
+import { dialogueData,scaleFactor } from "src/constants.js";
+import { k } from "src/kaboomCtx.js";
+import { displayDialogue, setCamScale } from "src/utils.js";
 
 
 k.loadSprite("spritesheet", "spritesheet.png", {
