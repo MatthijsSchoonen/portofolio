eb1b52d23114f134af03c3ccc4aee65cd74ad7a9
MatthijsSchoonen <MatthijsSchoonen@users.noreply.github.com>
2024-05-10 17:52:53 +0000

    deploy: 758dd4c70ed894bfab4a40a9553c45cf70634405

---
+++ constants.js
@@ -0,0 +1,9 @@
+export const scaleFactor = 4;
+
+
+export const dialogueData = {
+    pc: `Dit is mijn Computer hier programeer en game ik. ik programeer vaak games in Unity met c#. ik heb ook ervaring in python,php,html/css,javascript,typescript,sql. hier een link naar mijn github <a href="https://github.com/MatthijsSchoonen" target="_blank">Github</a>!`,
+    bed: `Hier slaap ik. Geweldige ideeen komen als ik op mijn bed lig`,
+    cv: `Dit is mijn bureau en daarop staat mijn cv. <a href="https://github.com/MATTHlJS/cv/blob/main/cv.pdf">Bekijken?</a>
+    Neem contact met me op via schoonenmatthijs@gmail.com als u mij een baan wilt aanbieden!`,
+  };
